051be74601be07937f43ce2f53d546f11cbee1df
Jason Anderson <jason@gearsandwires.com>
2020-08-20 22:13:54 -0700

    7.0.0

--- dist/Test/index.d.ts
+++ dist/Test/index.d.ts
@@ -1,9 +1,10 @@
+import { ManagedObject } from "../ManagedObject";
 declare function assert(value: boolean, failedMessage?: string): void;
 declare function assertIsDeepEqual(actual: any, expected: any, failedMessage?: string): void;
 declare function assertThrows(fn: () => any, failedMessage?: string): Promise<void>;
 export declare class Test {
     private static _allTests;
-    static define(label: string, fn: () => Promise<any> | void): void;
+    static define(label: string, fn: (emptyObject: ManagedObject) => Promise<any> | void): void;
     static assert: typeof assert;
     static assertThrows: typeof assertThrows;
     static assertIsDeepEqual: typeof assertIsDeepEqual;
